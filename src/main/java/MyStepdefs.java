fea9788c52fa082043440738cc76d9c97e972a0a
Alexey Andreikin <isqad.bro@gmail.com>
2019-10-02 16:14:24 +0300

    added feature file and simple step

---
+++ src/main/java/MyStepdefs.java
@@ -0,0 +1,23 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+
+    @Given("today is Sunday")
+    public void todayIsSunday() {
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void iAskWhetherItSFridayYet() {
+    }
+
+    @Then("I should be told {string}")
+    public void iShouldBeTold(String arg0) {
+    }
+
+    @Given("today is Friday")
+    public void todayIsFriday() {
+
+    }
+}
